dfc760bf2b4b99642dc505ef61c01735510951f2
xdloc <bretstiles0@gmail.com>
2023-04-27 21:55:49 +0700

    the initiation

---
+++ src/components/ShoppingList/ShoppingList.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="ShoppingList">
+        <pre v-text="$attrs"/>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Shoppinglist'
+    };
+</script>
---
+++ src/components/ShoppingListElement/ShoppingListElement.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="ShoppingListElement">
+        <pre v-text="$attrs"/>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Shoppinglistelement'
+    };
+</script>
